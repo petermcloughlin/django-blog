4857155ce49cab9633a401babf148c4a2a6ab2ca
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-09-13 12:20:23 +0000

    Add a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['8000-petermclough-djangoblog-lpbhdo0euzs.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
@@ -87,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

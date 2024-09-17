19de574f9bfe3f7f65d067cae8fb718b928e88a7
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-09-17 11:20:16 +0000

    Add generic listview for Posts

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
+    # queryset = Post.objects.all().order_by("created_on")
+    queryset = Post.objects.filter(status=1) # filter by status = 1 for those published posts
     template_name = "post_list.html"
